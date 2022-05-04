9a536749337debbe3e52d51a42edc8c6dafd18f3
luigser <luigser@gmail.com>
2022-05-04 15:30:09 +0200

    - Test on send message

--- packages/vfuse-core/src/components/networkManager.js
+++ packages/vfuse-core/src/components/networkManager.js
@@ -371,8 +371,9 @@ class NetworkManager{
 
     async send(data){
         try {
-            console.log(fflate.zlibSync((new TextEncoder().encode(JSON.stringify(data)))).length)
-            await this.libp2p.pubsub.publish(Constants.TOPICS.VFUSE_PUBLISH_CHANNEL.NAME, fflate.zlibSync((new TextEncoder().encode(JSON.stringify(data))), {level: 6}))/*LZUTF8.compress(JSON.stringify(data))*/
+            data = fflate.zlibSync((new TextEncoder().encode(JSON.stringify(data))), {level: 6})
+            console.log(`Sending ${data.length} bytes`)
+            await this.libp2p.pubsub.publish(Constants.TOPICS.VFUSE_PUBLISH_CHANNEL.NAME, data)/*LZUTF8.compress(JSON.stringify(data))*/
         }catch(e){
             console.log("Got some error during message sending on VFuse channel : " + e.message)
         }
