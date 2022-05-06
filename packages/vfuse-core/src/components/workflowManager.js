49d147eb9f185b644cc47e70c589b79e9e7f6c7d
luigser <luigser@gmail.com>
2022-05-06 17:31:00 +0200

    - replaced IPFS MFS - Improved result management

--- packages/vfuse-core/src/components/workflowManager.js
+++ packages/vfuse-core/src/components/workflowManager.js
@@ -321,6 +321,7 @@ class WorkflowManager{
                             entry.node.job.executorPeerId = this.identityManager.peerId
                             console.log("****************EXECUTED JOB*********************")
                             this.executedJobs.push(entry.node.job.id)
+                            console.log(this.executedJobs.length)
                             this.executedJobs.map( j => console.log(j))
                             console.log("****************END EXECUTED JOB*****************")
                             JobsDAG.setNodeState(
