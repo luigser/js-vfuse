cee4b4558a1fa4db765ed4c0130a79a6822eeded
luigser <luigser@gmail.com>
2022-05-07 16:02:00 +0200

    - Distributed balancing running workflows jobs selection

--- packages/vfuse-core/src/components/workflowManager.js
+++ packages/vfuse-core/src/components/workflowManager.js
@@ -330,8 +330,8 @@ class WorkflowManager{
             this.jobsExecutionQueue.map(j => console.log(j.node.id))
             console.log("REMOTE SELECTION")
             console.log("********************************************")
-            workflow_to_run.remoteSelectedJobs.map(j => console.log(j))*/
-            this.jobsExecutionQueue.map(j => console.log(j))
+            workflow_to_run.remoteSelectedJobs.map(j => console.log(j))
+            this.jobsExecutionQueue.map(j => console.log(j))*/
             if(node) {
                 if (!this.jobsExecutionQueue.find(e => e.node.id === node.id) && !workflow_to_run.remoteSelectedJobs.find(j => j === node.id)) {
                     //console.log(`Put in queue : ${node.id}`)
