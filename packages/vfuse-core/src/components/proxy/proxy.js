845101119336c07dd8e4aed4d9371f9694495a7a
Luigi Serra <luigser@gmail.com>
2022-06-10 16:07:57 +0200

    test

--- packages/vfuse-core/src/components/proxy/proxy.js
+++ packages/vfuse-core/src/components/proxy/proxy.js
@@ -30,7 +30,7 @@ class VFuseProxy{
             secure: props.certs.verify
         }).listen(props.pinning.proxyPort, "0.0.0.0");
         //WSS SWARM PROXY
-        httpProxy.createProxyServer({
+        let wssproxy = httpProxy.createServer({
             target: {
                 host: 'localhost',
                 port: props.bootstrap.wsPort,
@@ -41,6 +41,8 @@ class VFuseProxy{
                 key: fs.readFileSync(props.keyPemFile ? props.cert.keyPemFile :  path.join(__dirname, '..', '..', '..', '..', 'configuration', 'certs', 'key.pem'), 'utf8'),
                 cert: fs.readFileSync(props.certPemFile ? props.cert.certPemFile : path.join(__dirname, '..', '..', '..', '..', 'configuration', 'certs','cert.pem'), 'utf8')
             },
+            xfwd: true,
+            changeOrigin: true,
             secure: props.certs.verify
         }).listen(props.bootstrap.wsProxyPort, '0.0.0.0');
         //console.log("Proxy listening on wss://0.0.0.0:%s", props.bootstrap.wsProxyPort)
