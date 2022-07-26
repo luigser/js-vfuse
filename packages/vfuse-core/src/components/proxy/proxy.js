5b8d8a94b7c682cc66d9b751576c4bbb31c58ba5
Luigi Serra <luigser@gmail.com>
2022-07-26 14:36:48 +0200

    Test

--- packages/vfuse-core/src/components/proxy/proxy.js
+++ packages/vfuse-core/src/components/proxy/proxy.js
@@ -35,7 +35,8 @@ class VFuseProxy{
                 host: 'localhost',
                 port: props.bootstrap.wsPort,
             },*/
-            target: 'http://localhost:4003/',
+            //target: 'http://localhost:4003/',
+            target: 'http://172.16.149.150:4003/',
             ws : true,
             ssl: {
                 key: fs.readFileSync(props.keyPemFile ? props.cert.keyPemFile :  path.join(__dirname, '..', '..', '..', '..', 'configuration', 'certs', 'key.pem'), 'utf8'),
