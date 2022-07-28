9aab88dd85dadee0264049b8908aa96288896f7e
Giuseppe <giuseppe.dambrosio14@gmail.com>
2022-07-28 12:14:51 +0200

    Added MCWExperiment.js for VFuse benchmark

---
+++ benchmark/vfuse/MCWExperiment.js
@@ -0,0 +1,28 @@
+async function map(data){
+    let mapped = new Map()
+    let input = await VFuse.getDataFromUrl("https://raw.githubusercontent.com/giusdam/data/main/wordcount1-64.txt")
+    input.split(/\W+/).map(word => {
+        if(word !== "")
+            mapped.set(word, mapped.has(word) ? mapped.get(word) + 1 : 1)
+    })
+    return mapped
+}
+
+function reduce(data){
+    let result = new Map()
+    for(let d of data)
+        result.set(d.key, result.has(d.key) ? result.get(d.key) + d.value : d.value)
+
+    let max = result.entries().next().value
+    for(let value of result.entries()) {
+        if(value[1] > max[1])
+            max = value
+    }
+    return max
+}
+
+for(let i = 0; i < 1; i++){
+    await VFuse.addJob(map, [], null)
+}
+
+await VFuse.addJob(reduce, ['^map'])//wait for all reduce results and call combine
